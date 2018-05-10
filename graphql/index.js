d1b3c93204a7d57b8294d0a3c34ef330ffb93fde
DevArtemMrsh <mirosh1308@gmail.com>
2018-05-10 23:41:15 +0300

    minor changes

--- graphql/index.js
+++ graphql/index.js
@@ -12,8 +12,11 @@ const typeDefs = `
     users: [User]
     user(id: ID): User 
     boards: [Board]
+    board(id: ID): Board
     lists: [List]
+    list(id: ID): List
     tasks: [Task]
+    task(id: ID): Task
   }
 
   type User {
@@ -43,6 +46,19 @@ const typeDefs = `
     addUser(user: UserInput!): User
     updateUser(user: UserInput!, id: ID!): User
     removeUser(id: ID!): User
+
+    addBoard(board: BoardInput!): Board
+    updateBoard(board: BoardInput!, id: ID!): Board
+    removeBoard(id: ID!): Board
+
+    addList(list: ListInput!): List
+    updateList(list: ListInput!, id: ID!): List
+    removeList(id: ID!): List
+
+    addTask(task: TaskInput!): Task
+    updateTask(task: TaskInput!, id: ID!): Task
+    removeTask(id: ID!): Task
+
   }
 
   input UserInput {
@@ -50,10 +66,27 @@ const typeDefs = `
     email: String
   }
 
+  input BoardInput {
+    name: String!
+    lists: [ListInput]!
+  }
+
+  input ListInput {
+    name: String!
+    boardId: Int!
+    tasks: [TaskInput]!
+  }
+
+  input TaskInput {
+    title: String!
+    listId: Int!
+    author: UserInput
+  }
+
 `;
 
 const resolvers = {
-  
+
   Query: {
 
     async users() {
@@ -69,18 +102,34 @@ const resolvers = {
       return await Board.find({})
     },
 
+    async board(parent, args) {
+      const { id } = args;
+      await Board.findById(id);
+    },
+
     async lists() {
       return await List.find({})
     },
 
+    async list(parent, args) {
+      const { id } = args;
+      await Board.findById(id);
+    },
+
     async tasks() {
       return await Task.find({})
-    }
+    },
+
+    async list(parent, args) {
+      const { id } = args;
+      await Task.findById(id);
+    },
 
   },
 
   Mutation: {
 
+    // users
     async addUser(root, { user: userInput }) {
       const user = new User(userInput);
       user.password = 'pass';
@@ -90,10 +139,55 @@ const resolvers = {
       const { id, user } = args;
       return await User.findByIdAndUpdate(id, user, { new: true });
     },
-
     async removeUser(parent, args) {
       const { id } = args;
       return await User.findByIdAndRemove(id);
+    },
+
+    // boards
+    async addBoard(root, { board: BoardInput }) {
+      const board = new Board(BoardInput);
+      return await board.save();
+    },
+    async updateBoard(parent, args) {
+      const { id, board } = args;
+      return await Board.findByIdAndUpdate(id, board, { new: true });
+    },
+    async removeBoard(parent, args) {
+      const { id } = args;
+      return await Board.findByIdAndRemove(id);
+    },
+
+    // lists
+    async addList(root, { list: listInput }) {
+      const list = new List(listInput);
+      list.password = 'pass';
+      return await list.save();
+    },
+    async updateList(parent, args) {
+      const { id, list } = args;
+      return await List.findByIdAndUpdate(id, list, { new: true });
+    },
+
+    async removeList(parent, args) {
+      const { id } = args;
+      return await List.findByIdAndRemove(id);
+    },
+
+    // tasks
+    async addTask(root, { task: taskInput }) {
+      const task = new Task(taskInput);
+      task.password = 'pass';
+      return await task.save();
+    },
+    async updateTask(parent, args) {
+      const { id, task } = args;
+      return await Task.findByIdAndUpdate(id, task, { new: true });
+    },
+
+    async removeTask(parent, args) {
+      const { id } = args;
+      return await Task.findByIdAndRemove(id);
     }
 
   }
